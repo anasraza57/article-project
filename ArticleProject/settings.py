51ef93a9cc23b628ebbe60ff340b8db1445eafe2
anasraza57 <anassheikh57@gmail.com>
2022-06-23 15:51:21 -0400

    Update db settings

--- ArticleProject/settings.py
+++ ArticleProject/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'ArticleProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser2',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
